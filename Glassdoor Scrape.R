fa899f3f6b82399a11cc89846a501886f123d7b1
TannerSax <43909639+TannerSax@users.noreply.github.com>
2018-12-27 19:46:57 -0500

    Update Scrape for data analyst jobs

--- Glassdoor Scrape.R
+++ Glassdoor Scrape.R
@@ -4,13 +4,8 @@ library(dplyr)
 library(rvest)    
 library(stringr)
 
-#https://www.glassdoor.com/Job/robinhood-jobs-SRCH_KE0,9.htm
-#https://www.glassdoor.com/Job/ripple-jobs-SRCH_KE0,6.htm
-#https://www.glassdoor.com/Job/uber-jobs-SRCH_KE0,4.htm
-#https://www.glassdoor.com/Job/flexport-jobs-SRCH_KE0,8.htm
-
 # Open page in html
-url = 'https://www.glassdoor.com/Job/ripple-jobs-SRCH_KE0,6.htm'
+url = 'https://www.glassdoor.com/Job/new-york-data-analyst-jobs-SRCH_IL.0,8_IC1132348_KO9,21.htm'
 pg = read_html(url)
 
 # Get number of pages
@@ -21,28 +16,31 @@ get_last_page <- function(html){
     html_text()
   
   pages_data %>% 
-    regmatches(regexpr('[0-9]$|[0-9][0-9]$', pages_data))
+    regmatches(regexpr('[0-9][0-9][0-9]$', pages_data))
 }
 maxresults <- get_last_page()
 url_base <- str_remove(url, ".htm")
 
 # Create dataframe
-df <- map_df(1:maxresults, function(i) {
+df <- map_df(1:30, function(i) {
   
   Sys.sleep(sample(seq(1, 5, by=0.01), 1))   
   
   cat("boom! ")   #progress indicator
   
   list_of_pages <- read_html(paste0(url_base, "_IP", i, ".htm"))
-  
-  data.frame(rev.job = html_text(html_nodes(list_of_pages, '.jobLink')) %>% 
-               subset(. != ' no.logo.alt') %>% 
+
+  data.frame(Job = html_text(html_nodes(list_of_pages, '.jobTitle')) %>% 
                subset(. != ''), 
-             rev.loc = html_text(html_nodes(list_of_pages, '.loc')) %>% 
-             subset(. != ''),
-             rev.sum = html_text(html_nodes(list_of_pages, '.jl')),
+             Location = html_text(html_nodes(list_of_pages, '.loc')) %>% 
+               subset(. != ''),
+             Summary = html_text(html_nodes(list_of_pages, '.jl')),
              stringsAsFactors=F)
 })
 
+df <- df %>% 
+  mutate(python = ifelse(grepl("python", Summary, ignore.case = TRUE), 1, 0)) %>% 
+  mutate(sql = ifelse(grepl("sql", Summary, ignore.case = TRUE), 1, 0)) %>% 
+  mutate(java = ifelse(grepl("java", Summary, ignore.case = TRUE), 1, 0))
 
 
